6ff432190660f6ee1243215e585e9a6b2bd169d1
BLud26 <jx8385eq@go.minnstate.edu>
2026-04-28 19:03:34 -0500

    Added a few comments

--- src/main/java/com/ben/BloodDonorController.java
+++ src/main/java/com/ben/BloodDonorController.java
@@ -17,45 +17,62 @@ public class BloodDonorController {
 
     @GetMapping
     public String showDonorForm(Model model) {
+
+        // Put a new Donor object on the page so the form has something to bind to
         model.addAttribute("donor", new Donor());
+
+        // Show the form page
         return "donor_form";
     }
 
     @PostMapping("/submitDonorForm")
     public String processDonorForm(@Valid Donor donor, BindingResult bindingResult, Model model) {
 
+        // If validation failed, collect the errors and send them back to the form
         if (bindingResult.hasFieldErrors()) {
+
+            // Get all validation errors for the fields
             List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            // Convert errors into a simple map like { "age" -> "Age must be positive" }
             Map<String, String> validationErrorMap = ErrorUtils.createErrorMap(fieldErrors);
+
+            // Add the error messages to the model so the form can display them
             model.addAttribute("validationErrorMap", validationErrorMap);
 
             return "donor_form";
         }
-        else { // No errors. Is the donor eligible or not eligible
+        else {
+            // No validation errors. Now check eligibility rules
             String resultMessage;
             List<String> ineligibleReasons = new ArrayList<>();
 
+            // Check age requirement
             if (donor.getAge() < 16) {
                 ineligibleReasons.add("Age must be at least 16");
             }
 
+            // Check weight requirement
             if (donor.getWeight() < 110) {
                 ineligibleReasons.add("Weight must be at least 110lbs");
             }
 
-            // look at length of list. If there is data in the list, user is not eligible
-            // if list is empty, the user is eligible
-
+            // If the list is empty, they passed all checks
             if (ineligibleReasons.isEmpty()) {
                 resultMessage = "You may be eligible to donate blood";
             } else {
+                // Otherwise, they failed at least one requirement
                 resultMessage = "You are not eligible to donate blood";
+
+                // Send the list of reasons to the results page
                 model.addAttribute("ineligibleReasons", ineligibleReasons);
             }
 
+            // Add the final message to the model
             model.addAttribute("resultMessage", resultMessage);
+
+            // Show the results page
             return "results";
         }
-
     }
 }
